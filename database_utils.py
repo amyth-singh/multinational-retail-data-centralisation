fe44cc8957b40205d6fd931578df07e83d92083c
amyth-singh <theamithsingh@gmail.com>
2023-02-27 16:08:26 +0000

    changes

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,14 @@
 # Connects and uploads data to the database
-
+import yaml
 class DatabaseConnector:
-    pass
+    # reads credential info form yaml file
+    def read_db_cred():
+        pass
+
+    # reads from read_db_cred and returns an sqlalchemy database engine.
+    def init_db_engine():
+        pass
+
+    #
+    def list_db_tebles():
+        pass
