38870d120fd129bf03f7b0abf73ab10819b8623d
amyth-singh <theamithsingh@gmail.com>
2023-03-03 19:36:26 +0000

    adding methods

--- database_utils.py
+++ database_utils.py
@@ -1,10 +1,26 @@
+# Libraries used
+import yaml
+from yaml import Loader
+
 # Connect and upload data to the database
 class DatabaseConnector:
-    def read_db_cred():
-        pass
+    def read_db_creds():
+        yaml_file = open('db_creds.yaml', 'r')
+        data = yaml.load(yaml_file, Loader=Loader)
+        return data
 
     def init_db_engine():
-        pass
+        data = DatabaseConnector.read_db_creds()
+        database_type = 'postgresql'
+        dbapi = 'psycopg2'
+        host = data['RDS_HOST']
+        password = data['RDS_PASSWORD']
+        user = data['RDS_USER']
+        database = data['RDS_DATABASE']
+        port = data['RDS_PORT']
+        engine = create_engine(f"{database_type}+{dbapi}://{user}:{password}@{host}:{port}/{database}")
+        connect = engine.connect()
+        return connect
 
     def list_db_tebles():
         pass
